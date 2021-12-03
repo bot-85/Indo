fbd5170732dda5301f9e5e2a1ba5d9a6e8051d83
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-12-04 06:24:21 +0700

    Update Indo.py

--- Indo.py
+++ Indo.py
@@ -172,7 +172,7 @@ def menu():
     print(" \x1b[1;92m[\x1b[1;93m04\x1b[1;92m] \x1b[1;92mCRACK DARI POSTINGAN")
     print(" \x1b[1;92m[\x1b[1;93m05\x1b[1;92m] \x1b[1;92mCHECK OPSI HASIL CRACK")
     print(" \x1b[1;92m[\x1b[1;93m06\x1b[1;92m] \x1b[1;92mCHECK HASIL CRACK")
-     print(" \x1b[1;92m[\x1b[1;93m07\x1b[1;92m] \x1b[1;92mLAPORAN SCRIPT")
+    print(" \x1b[1;92m[\x1b[1;93m07\x1b[1;92m] \x1b[1;92mLAPORAN SCRIPT")
     print(" [%s00%s]. REMOVE TOKEN"%(M,N))
     asw = raw_input("\n [?] pilih  : \x1b[1;92m")
     if asw == "":
